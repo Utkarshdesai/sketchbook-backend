35dc419a00eb7cf630e3eac0342968bb48668a29
Utkarsh <desaiutkarsh542@gmail.com>
2024-04-14 22:40:43 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const http = require('http')
+const { createServer } = require("http");
+const {Server} = require('socket.io')
+const cors = require('cors')
+const app = express();
+app.use(cors())
+const httpServer = createServer(app);
+
+const io = new Server (httpServer , {
+    cors : {
+        origin: "http://localhost:3000",
+        credentials: true
+    }
+})
+
+io.on('connection' , (socket) => {
+   console.log('server is started')
+
+   socket.on('beginPath' , (arg)=>{
+    socket.broadcast.emit('beginPath', arg)
+   })
+
+   socket.on('drawLine', (arg) => {
+    
+     socket.broadcast.emit('drawLine', arg)
+   })
+
+    socket.on('changeConfig', (arg) => {
+        
+       socket.broadcast.emit('changeConfig', arg)
+   })
+})
+
+httpServer.listen(8000, () => {
+    console.log(`Socket.io server is running on port 4000`);
+  });
+
+app.listen(8004,()=>{
+    console.log('welcome')
+})
